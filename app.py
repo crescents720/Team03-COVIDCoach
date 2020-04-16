7d7dae1ca66222025e131cada96eaac5b085f73b
yy30537 <yang30537@gmail.com>
2020-04-16 00:03:34 -0700

    added some html files to templates to test routing

--- app.py
+++ app.py
@@ -6,6 +6,20 @@ def index():
     title = 'COVID Website'
     return render_template('index.html', title=title)
 
+@app.route('/news')
+def news_page():
+    title = 'COVID Website Get News'
+    return render_template('news.html', title=title)
+
+@app.route('/help')
+def help_page():
+    title = 'COVID Website Get Help'
+    return render_template('help.html', title=title)
+
+@app.route('/board')
+def board_page():
+    title = 'COVID Website Message Board'
+    return render_template('board.html', title=title)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=3000)
