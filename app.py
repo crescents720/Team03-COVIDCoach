c269f3acb7dd0d055d0c50974e190927d3927563
yy30537 <yang30537@gmail.com>
2020-04-16 21:03:03 -0700

    Thanks to newsapi now the Get News page gets news. Images contain urls btw

--- app.py
+++ app.py
@@ -1,25 +1,57 @@
 from flask import Flask, render_template
+from newsapi import NewsApiClient
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    title = 'COVID Website'
+    title = 'COVID Coach'
     return render_template('index.html', title=title)
+
 
 @app.route('/news')
 def news_page():
-    title = 'COVID Website Get News'
-    return render_template('news.html', title=title)
+    title = 'COVID Coach Get News'
+    
+    newsapi = NewsApiClient(api_key="31119c92ed55433d9083373aba50327b")
+    topheadlines = newsapi.get_top_headlines(q='covid',language='en')
+ 
+    articles = topheadlines['articles']
+ 
+    news_description = []
+    news_title = []
+    news_image = []
+    news_url = []
+ 
+    for i in range(len(articles)):
+        myarticles = articles[i]
+ 
+        news_title.append(myarticles['title'])
+        news_description.append(myarticles['description'])
+        news_image.append(myarticles['urlToImage'])
+        news_url.append(myarticles['url'])
+ 
+    newsl_list = zip(news_title, news_description, news_image, news_url)
+ 
+    return render_template('news.html', context=newsl_list, title=title)
+
+
+
+
 
 @app.route('/help')
 def help_page():
-    title = 'COVID Website Get Help'
+    title = 'COVID Coach Get Help'
     return render_template('help.html', title=title)
 
 @app.route('/board')
 def board_page():
-    title = 'COVID Website Message Board'
+    title = 'COVID Coach Message Board'
     return render_template('board.html', title=title)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=3000)
+
+
+
+
