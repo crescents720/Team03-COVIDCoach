eaec764589011ba9c1a4c0792b5aa3c3cdd9af86
Yiqiao Lu <daphnelyq@gmail.com>
2020-04-17 15:19:49 -0700

    add navbar bootstrap

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 from newsapi import NewsApiClient
 import pandas as pd
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
 @app.route('/')
 def index():
@@ -13,27 +15,27 @@ def index():
 @app.route('/news')
 def news_page():
     title = 'COVID Coach Get News'
-    
+
     newsapi = NewsApiClient(api_key="31119c92ed55433d9083373aba50327b")
     topheadlines = newsapi.get_top_headlines(q='covid' or 'coronavirus',language='en',page_size=100)
 
     articles = topheadlines['articles']
- 
+
     news_description = []
     news_title = []
     news_image = []
     news_url = []
- 
+
     for i in range(len(articles)):
         myarticles = articles[i]
- 
+
         news_title.append(myarticles['title'])
         news_description.append(myarticles['description'])
         news_image.append(myarticles['urlToImage'])
         news_url.append(myarticles['url'])
- 
+
     newsl_list = zip(news_title, news_description, news_image, news_url)
- 
+
     return render_template('news.html', context=newsl_list, title=title)
 
     dataframe = pd.DataFrame(articles)
